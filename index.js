9647f06dcd37b5afbe30735ea8297bcf1ec5f57c
Sibabale <sibabale@projectcodex.co>
2017-09-17 12:59:58 +0200

    can use routes to find and add data

--- index.js
+++ index.js
@@ -14,19 +14,40 @@ const app = express();
 const port = process.env.PORT || 8083
 const local_db = process.env.MONGO_DB_URL || ('mongodb://localhost/shoe_api');
 
-mongoose.connect(local_db, {useMongoClient: true}, function(error) {
-    if (error) {
-      console.log('DB connection failed: ' + error);
-    }else{
-      console.log('DB connection successful');
-    }
+mongoose.connect(local_db, {
+  useMongoClient: true
+}, function(error) {
+  if (error) {
+    console.log('DB connection failed: ' + error);
+  } else {
+    console.log('DB connection successful');
+  }
 })
+
+const shoeModel = mongoose.model('shoeModel', {
+  id: Number,
+  color: String,
+  brand: String,
+  cash: Number,
+  size: Number,
+  in_stock: Number
+});
+
+
 
 mongoose.Promise = global.Promise;
 
 app.use(flash());
 
-app.use(session({secret: 'keyboard cat', cookie: {maxAge: 60000 * 30 },proxy: 'true', resave: 'true', saveUninitialized: 'true'}));
+app.use(session({
+  secret: 'keyboard cat',
+  cookie: {
+    maxAge: 60000 * 30
+  },
+  proxy: 'true',
+  resave: 'true',
+  saveUninitialized: 'true'
+}));
 
 app.use(express.static('public'));
 
@@ -40,26 +61,54 @@ app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
   res.render('index');
 });
-app.get('/api/shoes', function(req,res){
-  res.render('allShoes');
+app.get('/api/shoes', function(req, res) {
+  const newShoeEntry = new shoeModel({
+    // id: Number,
+    color: "Green",
+    brand: "Nike",
+    cash: 1200,
+    size: 6,
+    in_stock: 500
+  });
+
+  newShoeEntry.save();
+  shoeModel.find({}, function(error, results) {
+    if (error) {
+      console.log(error);
+    } else {
+      // console.log(color.results);
+      res.json(results);
+    }
+  })
 });
-app.get('/api/shoes/brand/:brandname', function(req,res){
-  res.render('index');
+app.get('/api/shoes/brand/:brandname', function(req, res) {
+  const shoeBrand = req.params.brandname;
+
+  res.json({
+    Brand_Name: shoeBrand,
+  });
 });
-app.get('/api/shoes/brand/:brandname/size/:size', function(req,res){
-  res.render('index');
+app.get('/api/shoes/brand/:brandname/size/:size', function(req, res) {
+  const shoeBrand = req.params.brandname;
+  const shoeSize = req.params.size;
+
+  res.json({
+    Brand_Name: shoeBrand,
+    Brand_Size: shoeSize
+  });
 });
-app.post('/api/shoes/sold/:id', function(req,res){
-  res.render('index');
+app.post('/api/shoes/sold/:id', function(req, res) {
+  const soldShoes = req.params.id;
+  res.json(soldShoes);
 });
-app.post('/api/shoes', function(req,res){
+app.post('/api/shoes', function(req, res) {
   // console.log(data);
   res.render('allShoes');
 });
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log('App running on port: ' + port);
 });
