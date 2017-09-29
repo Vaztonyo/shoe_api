cdfa5ce046b3495871a510632a4e67702ee42e09
Sibabale <jojasibabale@gmail.com>
2017-09-29 14:55:24 +0200

    can deduct shoes sold

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const flash = require('express-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const ObjectId = require('mongodb').ObjectId;
 
-// exports module files
-const data = require('./data')
 
 
 const app = express();
+
 app.use(flash());
 
 app.set('trust proxy', 1);
@@ -24,6 +24,7 @@ app.use(session({
   }
 }));
 const port = process.env.PORT || 8083
+
 const local_db = process.env.MONGO_DB_URL || ('mongodb://localhost/shoe_api');
 
 mongoose.connect(local_db, {
@@ -82,53 +83,70 @@ app.get('/api/shoes', function(req, res) {
       console.log(error);
     } else {
       console.log(results);
-      res.render('allShoes',{
-          shoeInfo: results
-      });
+      res.json(results);
     }
   })
 });
 app.get('/api/shoes/brand/:brandname', function(req, res) {
   // capitalizing route parameters
-  const capitalize = req.params.brandname.substring(0,1);
-  const toUpperCase = req.params.brandname.substring(0,1).toUpperCase();
+  const capitalize = req.params.brandname.substring(0, 1);
+  const toUpperCase = req.params.brandname.substring(0, 1).toUpperCase();
 
   const brandname = req.params.brandname.replace(capitalize, toUpperCase);
-  shoeModel.find({brand:brandname}, function(error, results){
+  shoeModel.find({
+    brand: brandname
+  }, function(error, results) {
     if (error) {
       console.log(error);
-    }else {
-      res.render('allShoes',{
-          shoeInfo: results
-      });
+    } else {
+      res.json(results);
     }
   });
 });
 app.get('/api/shoes/brand/:brandname/size/:size', function(req, res) {
   // capitalizing route parameters
-  const capitalize = req.params.brandname.substring(0,1);
-  const toUpperCase = req.params.brandname.substring(0,1).toUpperCase();
+  const capitalize = req.params.brandname.substring(0, 1);
+  const toUpperCase = req.params.brandname.substring(0, 1).toUpperCase();
 
   const brandname = req.params.brandname.replace(capitalize, toUpperCase);
   const brandsize = req.params.size;
 
-  shoeModel.find({brand:brandname, size:brandsize}, function(error, results){
+  shoeModel.find({
+    brand: brandname,
+    size: brandsize
+  }, function(error, results) {
     if (error) {
       console.log(error);
-    }else {
-      res.render('allShoes',{
-          shoeInfo: results
-      });
+    } else {
+      res.json(results);
     }
   });
 });
 app.post('/api/shoes/sold/:id', function(req, res) {
   const soldShoes = req.params.id;
+
+  shoeModel.findOneAndUpdate({
+      _id: ObjectId(soldShoes)
+    }, {
+      $inc: {
+        in_stock: -1
+      }
+    }, {
+      upsert: false
+    },
+    function(error, results) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(results);
+      }
+    })
   res.json(soldShoes);
+  res.json(ObjectId(soldShoes).str);
 });
 app.get('/admin', function(req, res) {
   // console.log(data);
-  res.render('admin');
+  res.json('admin');
 });
 
 app.post('/api/shoes', function(req, res) {
@@ -149,15 +167,15 @@ app.post('/api/shoes', function(req, res) {
     in_stock: in_stock
   });
 
-  newShoeEntry.save(); 
+  newShoeEntry.save();
 });
 
 app.post('/available-stock', function(req, res) {
-  shoeModel.find({}, function(error, results){
+  shoeModel.find({}, function(error, results) {
     if (error) {
       console.log(error);
-    }else {
-      res.render('admin',{
+    } else {
+      res.json('admin', {
         shoeInfo: results
       });
     }
@@ -166,12 +184,14 @@ app.post('/available-stock', function(req, res) {
 app.post('/delete', function(req, res) {
 
   const id = req.body.id;
-  shoeModel.deleteOne({id:id}, function(error, results){
+  shoeModel.deleteOne({
+    id: id
+  }, function(error, results) {
     if (error) {
       console.log(error);
-    }else {
-       req.flash('deleted', 'You have successfully deleted ' + id);
-      res.render('admin')
+    } else {
+      req.flash('deleted', 'You have successfully deleted ' + id);
+      res.json('admin')
     }
   });
   // console.log(brand );
